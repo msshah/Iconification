bec0a88b9a31bd61de099100255141c48f3008fa
Mian Safyan Shah <safyanshah@gmail.com>
2016-04-09 23:29:25 +0500

    Added files via upload

---
+++ Application.java
@@ -0,0 +1,11 @@
+public class Application {
+
+	// dc
+	public Application() {
+		Iconification iconification = new Iconification();
+	}
+	// main
+	public static void main(String[] args) {
+		Application app = new Application();
+	} // end of main
+} // end of class
