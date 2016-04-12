9233474941c7dc13a89a4805fa5f90d3a04531cd
Mian Safyan Shah <safyanshah@gmail.com>
2016-04-12 12:12:59 +0500

    Added files via upload

---
+++ Properties.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2016 Mian Safyan Shah
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ **/
+
+import java.awt.Dimension;
+
+public final class Properties {
+
+    public static int[] iResolutions = {16, 32, 64, 128, 256, 512};
+    public static Dimension dimensionUniversalFrameSize = new Dimension(500, 400);
+    
+    public static String sEXIT_BUTTON_TITLE = "Exit";
+    public static String sIMAGE_PLACEHOLDER_TEXT = "image goes here";
+    
+    public static String sICONSET_FOLDER_NAME = "AppIcon.iconset";
+    
+} // end of class Strings
